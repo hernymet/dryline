31d841081ea1997d59ddbf9300b31c44bce66a1d
hernymet <hernymet@gmail.com>
2016-04-26 11:47:24 -0300

    Modificado era.py

--- era.py
+++ era.py
@@ -7,10 +7,10 @@ server = ECMWFDataServer()
 for year in range(2015, 2016):
  print 'YEAR ',year
  for month in range(01,02):
- lastday1=calendar.monthrange(year,month)
- lastday=lastday1[1]
- bdate="%s%02d01"%(year,month)
- edate="%s%02d%s"%(year,month,lastday)
+  lastday1=calendar.monthrange(year,month)
+  lastday=lastday1[1]
+  bdate="%s%02d01"%(year,month)
+  edate="%s%02d%s"%(year,month,lastday)
   print "######### ERA-interim  #########"
   print 'get data from ', bdate,' to ',edate,' (YYYYMMDD)'
   print "################################"
